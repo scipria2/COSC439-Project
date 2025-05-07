96234b9dc002a8a1be9bb2d065fa3842f5464e97
sciprian <scipria2@students.towson.edu>
2025-05-06 20:59:32 -0400

    The initial USB detect driver

---
+++ devicedriver.c
@@ -0,0 +1,51 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/usb.h>
+
+
+//missing USB interface constants
+#define USB_CLASS_MASS_STORAGE 0x86
+#define USB_SC_SCSI 0x06
+#define USB_PR_BULK
+
+//USB driver struct
+static struct usb_driver usb_crypto_driver ={
+    .name = "USB Crypo Driver",
+    .id_table = usb_table,
+    .probe = usb_crypto_probe,
+    .disconnect = usb_crypto_disconnect,
+};
+
+//called when the USB device is removed
+static void usb_crypto_disconnect (struct usb_interface *interface)
+{
+    printk(KERN_INFO "USB Crypto Driver: USB Device removed\n");
+}
+
+
+static struct usb_device_id usb_table[] = 
+{
+ {USB_INTERFACE_INFO(USB_CLASS_MASS_STORAGE, USB_SC_SCSI, USB_PR_BULK) },
+ {}
+}; 
+MODULE_DEVICE_TABLE(usb, usb_table)
+
+//Module init
+static int __init usb_crypto_init(void)
+{
+    printk(KERN_INFO "USB Crypto Driver: Module loaded\n")
+    return usb_register(&usb_crypto_driver);
+}
+
+//Module exit
+static void __exit usb_crypto_exit(void)
+{
+    usb_deregister(&usb_crypto_driver);
+    printk(KERN_INFO "USB Crypto Driver: Module unloaded\n");
+}
+
+module_init(usb_crypto_init);
+module_exit(usb_crypto_exit);
+
+MODULE_AUTHOR("Stephanie Ciprian");
+MODULE_DESCRIPTION("A USB driver that encrypts and decrypts data in C")
